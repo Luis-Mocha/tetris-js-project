9c6c6debf6f840dabcfb8f5f66c5654fe9662a7b
Luis-Mocha <luismocha97@gmail.com>
2023-09-04 15:29:40 +0200

    creation grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-console.log('hello world heheh');
+document.addEventListener('DOMContentLoaded', startAll());
+
+function startAll() {
+    let grid = document.getElementById('grid');
+
+    for (let index = 0; index < 200; index++) {
+        grid.innerHTML += `<div class="block-${index}"></div>`
+    }
+    
+}
