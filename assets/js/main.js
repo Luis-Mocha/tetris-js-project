de5065958833cc8dd010fe3d0a7c77fcd8891e1d
Luis-Mocha <luismocha97@gmail.com>
2023-09-04 17:19:40 +0200

    tetrominoes array / function draw()

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,4 +15,61 @@ function startAll() {
 
     let squares = Array.from(document.querySelectorAll('.square'));
     console.log(squares);
+
+
+    // defining all tetraminos
+    const width = 10;
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ];
+
+    const zTetromino = [
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1]
+    ];
+
+    const tTetromino = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+    ];
+
+    const oTetromino = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+    ];
+
+    const iTetromino = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+    ];
+
+    const allTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+    
+
+    let currentPosition = 4; // where to start a tetromino (x-axis)
+    let currentRotation = 0;
+
+    // randomly select a Tetromino
+    let random = Math.floor(Math.random()*allTetrominoes.length);
+    let currentTetro = allTetrominoes[random][currentRotation]; // [type of tetramino][rotation]
+
+    // drawing the tetromino
+    function draw() {
+        currentTetro.forEach(block => {
+            squares[currentPosition + block].classList.add('tetromino');
+        })
+    };
+    draw();
+
 }
