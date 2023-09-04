747690d13b0697079a2b98617a49c6b4f0912182
Luis-Mocha <luismocha97@gmail.com>
2023-09-04 16:50:17 +0200

    grid squares

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,16 @@ document.addEventListener('DOMContentLoaded', startAll());
 function startAll() {
     let grid = document.getElementById('grid');
 
-    for (let index = 0; index < 200; index++) {
-        grid.innerHTML += `<div class="block-${index}"></div>`
+    for (let y = 1; y < 21; y++) {
+
+        for (let x = 1; x < 11; x++) {
+
+            grid.innerHTML += `<div class="square square-${x}-${y}"></div>`
+
+        }
+        
     }
-    
+
+    let squares = Array.from(document.querySelectorAll('.square'));
+    console.log(squares);
 }
